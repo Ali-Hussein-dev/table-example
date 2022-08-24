9b5e2b1ec405697ef374bd47d36e1d4b40e4b220
Ali <ali.hussein.pre@gmail.com>
2022-08-24 12:38:14 +0200

    feat: add pagination

--- src/components/TableExample.tsx
+++ src/components/TableExample.tsx
@@ -2,15 +2,18 @@ import { Table } from "@mantine/core";
 import { useReactTable, flexRender } from "@tanstack/react-table";
 import {
   createColumnHelper,
-  getCoreRowModel,
   SortingState,
   getSortedRowModel,
+  getCoreRowModel,
+  getPaginationRowModel,
 } from "@tanstack/table-core";
 import {
   BiChevronUp,
   BiChevronDown,
   BiChevronRight,
   BiChevronLeft,
+  BiFemale,
+  BiMale,
 } from "react-icons/bi";
 import { TbSelector } from "react-icons/tb";
 import * as React from "react";
@@ -33,18 +36,22 @@ const columns = [
   columnHelper.accessor("firstname", {
     cell: (info) => info.getValue(),
     footer: (info) => info.column.id,
+    header: "Vorname",
   }),
   columnHelper.accessor("lastname", {
     cell: (info) => info.getValue(),
     footer: (info) => info.column.id,
-  }),
-  columnHelper.accessor("email", {
-    cell: (info) => info.getValue(),
-    footer: (info) => info.column.id,
+    header: "Nachname",
   }),
   columnHelper.accessor("gender", {
     cell: (info) => info.getValue(),
     footer: (info) => info.column.id,
+    header: "Geschlecht",
+  }),
+  columnHelper.accessor("email", {
+    cell: (info) => info.getValue(),
+    footer: (info) => info.column.id,
+    header: "Email",
   }),
 ];
 //======================================
@@ -56,8 +63,10 @@ const TableExample = ({ data }: { data: any[] }) => {
     columns,
     state: { sorting },
     onSortingChange: setSorting,
-    getSortedRowModel: getSortedRowModel(),
+    // pipeline
     getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
   });
   const { getHeaderGroups, getRowModel } = table;
   //======================================return
@@ -100,22 +109,47 @@ const TableExample = ({ data }: { data: any[] }) => {
             <tr key={row.id}>
               {row.getVisibleCells().map((cell) => (
                 <td key={cell.id}>
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  {cell.column.columnDef.header === "Geschlecht" ? (
+                    cell.getValue() === "male" ? (
+                      <BiMale size="25" className="text-sky-600" />
+                    ) : (
+                      <BiFemale size="25" className="text-pink-600" />
+                    )
+                  ) : (
+                    flexRender(cell.column.columnDef.cell, cell.getContext())
+                  )}
                 </td>
               ))}
             </tr>
           ))}
         </tbody>
-        <tfoot className="border-t">
-          <div className="w-full h-10 row-between">
-            <div className="row-start gap-x-1 ">
-              <BiChevronLeft size="30" />
-              <BiChevronRight size="30" />
-            </div>
-            {/* <div className="w-full">page 1 of page 20</div> */}
-          </div>
-        </tfoot>
       </Table>
+      <div className="w-full border-t">
+        <div className="w-full h-12 row-between pag-x-2 ">
+          <div className="row-start gap-x-1 ">
+            <button
+              className="btn btn-square btn-sm"
+              onClick={() => table.previousPage()}
+              disabled={!table.getCanPreviousPage()}
+            >
+              <BiChevronLeft size="30" />
+              <span className="sr-only">previous page</span>
+            </button>
+            <button
+              disabled={!table.getCanNextPage()}
+              className="btn btn-square btn-sm"
+              onClick={() => table.nextPage()}
+            >
+              <BiChevronRight size="30" />
+              <span className="sr-only">next page</span>
+            </button>
+          </div>
+          <div className="px-2 font-semibold">
+            Seite {table.getState().pagination.pageIndex + 1} von{" "}
+            {table.getPageCount()}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
